7540b1401ca9c5735be4199d8578b0e208e2a440
HamimBhai742 <omagootruelove147@gmail.com>
2025-11-14 08:27:48 +0600

    get all doctor meta data:

--- src/modules/meta/meta.services.ts
+++ src/modules/meta/meta.services.ts
@@ -1,15 +1,15 @@
-import { PaymentStatus } from '@prisma/client';
+import { PaymentStatus, Role } from '@prisma/client';
 import { prisma } from '../../config/prisma.config';
 import { AppError } from '../../error/coustome.error';
 import { IJWTPayload } from '../../types/interface';
 
 const fetchDashboardData = (user: IJWTPayload) => {
   switch (user.role) {
-    case 'ADMIN':
+    case Role.ADMIN:
       return getAdminMetadata();
-    case 'DOCTOR':
-      return 'Doctor';
-    case 'PATIENT':
+    case Role.DOCTOR:
+      return getDoctorMetadata(user);
+    case Role.PATIENT:
       return 'Patient';
     default:
       throw new AppError('Role not found', 404);
@@ -74,8 +74,69 @@ const pieChartData = async () => {
   return data;
 };
 
+const getDoctorMetadata = async (user: IJWTPayload) => {
+  const doctor = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      email: user.email,
+    },
+  });
 
+  const totalAppoinment = await prisma.appointment.count({
+    where: {
+      doctorId: doctor.id,
+    },
+  });
 
+  const totalPatient = await prisma.appointment.groupBy({
+    by: ['patientId'],
+    _count: {
+      id: true,
+    },
+    where: {
+      doctorId: doctor.id,
+    },
+  });
+
+  const totalreview = await prisma.review.count({
+    where: {
+      doctorId: doctor.id,
+    },
+  });
+
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: {
+      amount: true,
+    },
+    where: {
+      appointment: {
+        doctorId: doctor.id,
+      },
+      status: PaymentStatus.PAID,
+    },
+  });
+
+  const appoinmentsStatusCount = await prisma.appointment.groupBy({
+    by: ['status'],
+    _count: {
+      id: true,
+    },
+    where: {
+      doctorId: doctor.id,
+    },
+  });
+
+  const data = appoinmentsStatusCount.map(({ status, _count }) => ({
+    status,
+    count: _count.id,
+  }));
+  return {
+    totalAppoinment,
+    totalPatient,
+    totalreview,
+    totalRevenue,
+    appoinmentStatusCount: data,
+  };
+};
 
 export const metaServices = {
   fetchDashboardData,
